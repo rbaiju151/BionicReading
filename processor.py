1d787df588b3bb4b0c22ded5bbbdb36b18106c07
rbaiju151 <rbaiju@andrew.cmu.edu>
2026-03-15 18:52:27 -0400

    Update processor.py

--- processor.py
+++ processor.py
@@ -5,7 +5,3 @@ def bionic_bold(matchobj):
     text = matchobj.group(0)
     bold_length = math.ceil(len(text) / 2)
     return f'<b>{text[:bold_length]}</b>{text[bold_length:]}'
-
-text = input("Enter some text: ")
-
-print(re.sub('[a-zA-Z]+', bionic_bold, text))
